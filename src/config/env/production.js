664c6aea050236d450d1da5839153fb1c63ce181
DavidSilvaPaiva <david654321123456@gmail.com>
2020-01-23 16:43:39 -0300

    version 1.7

--- src/config/env/production.js
+++ src/config/env/production.js
@@ -1,7 +1,7 @@
 class Config {
   constructor() {
     this.env = 'production';
-    this.PORT = 3000;
+    this.PORT = process.env.PORT || 3000;
     this.API_BASE = '/api';
     this.MONGODB_URL = 'mongodb://up9sp9opnqktvbwa8wog:GGGS9RVl7TdYc6WDWLgD@bxtwflrzufza7zt-mongodb.services.clever-cloud.com:27017/bxtwflrzufza7zt';
   }
